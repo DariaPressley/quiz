5b7c592f555afa2ee60f0d2db6b94c82d00616a0
DariaPressley <daria.mik@gmail.com>
2023-09-14 23:04:30 -0700

    first push - code still not complete

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# quiz
-Online Quiz 
+# Quiz
+
+## Description
+
+This page generates a coding quiz challenge for users to test their skills in five timed questions.
+
+## Acceptance Criteria
+
+It is done when...
+
+When the user clicks the button to generate a password.
+
+They are presented with a series of prompts for password criteria.
+
+They select which criteria to include in the password - length of characters, character types, and special characters.
+
+The user answers each prompt and at least one character type is selected from the desired prompts.
+
+All prompts are answered and a password is generated that matches the selected criteria.
+
+The password is generated and displayed in an alert or written to the page
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
+
+![Alt text](coding-quiz.png)
---
+++ script.js
@@ -0,0 +1,41 @@
+var quizContainer = document.getElementById("quiz-container");
+var questionElement = document.getElementById("question");
+var optionsElement = document.getElementById("answer");
+var resultContainer = document.getElementById("result-container");
+var scoreElement = document.getElementById("score");
+var instructions = document.getElementsByClassName("instructions"); 
+var quizQuestion= [questionOne, questionTwo, questionThree, questionFour, questionFive]
+
+var questionOne = {question:"What does API stand for?",
+answerOne:["Application Programming Interface","Application Painting Interior","Appendix Pending Initiation","none of the above"],
+correctIndex: 0};
+
+var questionTwo = {question:"What is the purpose of a README.md?",
+answerTwo: ["none of the above","This document is not needed.", "To store extra code.","To serve as a warning for risky websites."],
+correctIndex: 0};
+
+var questionThree = {question:"What does Javascript do?",
+answerThree: ["Give the page glitter.","Nothing.", "Hides browser history.","Make web pages interactive."],
+correctIndex: 3};
+
+var questionFour = {question:"Which is NOT a kind of variable?",
+answerFour: ["string", "starred", "boolean", "number"],
+correctIndex: 1};
+
+var questionFive = {question:"What kind of elements does a parent element contain?",
+answerFive: ["pet","child","dress","interior"],
+correctIndex: 1};
+
+var currentQuestionIndex = 0;
+var score = 0;
+
+var startButton = document.getElementById("startbutton");
+startButton.addEventListener('click', function () {
+    startQuiz();
+});
+
+function startQuiz() {
+    startButton.style.display = "none";
+    instructions.style.display= "none";
+    displayQuestion();
+}
